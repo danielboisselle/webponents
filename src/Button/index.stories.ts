16c0c3efdef2b6eeecea7d915e7fea7df8d9876e
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2023-01-07 00:53:53 -0500

    Add storybook & decorators support

--- src/Button/index.stories.ts
+++ src/Button/index.stories.ts
@@ -1,36 +1,24 @@
+import { Story } from '@storybook/web-components';
 import { html } from 'lit-html';
-import { withKnobs, text, boolean, withWebComponentsKnobs } from '@open-wc/demoing-storybook';
-
 import './index'
 
 export default {
-  title: 'Button',
-  decorators: [withKnobs, withWebComponentsKnobs],
-  parameters: {
-    controls: {
-      active: { type: 'boolean' },
-      disabled: { type: 'boolean' },
-      loading: { type: 'boolean' },
-      label: { type: 'text' },
-      size: { type: 'text' },
-    },
-  },
-};
+  title: 'Example/Button',
+  component: 'wp-button'
+}
 
-export const Default = () => {
-  const label = text('Label', 'Click me');
-  const disabled = boolean('Disabled', false);
-  const loading = boolean('Loading', false);
-  const active = boolean('Active', false);
-  const size = text('Size', 'medium');
-
-  return html`
-    <wp-button
-      .label=${label}
-      ?disabled=${disabled}
-      ?loading=${loading}
-      ?active=${active}
-      .size=${size}
+const Template: Story = (args) => html`
+  <wp-button
+    .label="${args.label}"
+    .loading="${args.loading}"
+    .disabled="${args.disabled}"
     ></wp-button>
-  `;
+`;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  label: 'My Label',
+  loading: false,
+  disabled: false,
 };
